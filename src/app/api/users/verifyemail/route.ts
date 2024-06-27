c45c8140ffbe553367243c4f33715cdce6283ab2
rumman99 <tasnimrumman369@gmail.com>
2024-06-27 23:51:39 +0600

    create verifyemail route

--- src/app/api/users/verifyemail/route.ts
+++ src/app/api/users/verifyemail/route.ts
@@ -1,4 +1,5 @@
 import { connectDB } from "@/dbConfig/dbConfig";
+import { User } from "@/models/user.model";
 import { NextRequest, NextResponse } from "next/server";
 
 
@@ -7,6 +8,21 @@ connectDB();
 
 export async function POST(req:NextRequest){
     try {
+        const {token}= await req.json();
+        console.log(token);
+
+        const user= await User.findOne({verifyToken: token, verifyTokenExpiry: {$gt: Date.now()}})
+
+        if(!user){
+            return NextResponse.json({error:"Invalid Token or Expiry Date!!!"}, {status:400})
+        }
+        user.isVerified= true,
+        user.verifyToken= undefined
+        user.verifyTokenExpiry= undefined
+
+        await user.save();
+
+        return NextResponse.json({message: "Email Verification Successful, Now you can Login", success:true}, {status:200})
         
     } 
     catch (error:any) {
--- src/utils/generateToken.ts
+++ src/utils/generateToken.ts
@@ -1,5 +1,4 @@
 import { User } from "@/models/user.model"
-import bcrypt from "bcryptjs";
 
 // hashing for Verify Token //
 export const generateToken= async(emailType:any, userId:any, hashedToken:any)=>{
